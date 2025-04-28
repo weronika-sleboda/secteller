f4770fa69c122f82f806b97eca9527e911c29648
Weronika Sleboda <weronika_sleboda@hotmail.com>
2025-04-28 13:16:57 +0200

    fixed some errors

--- config/settings.py
+++ config/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t3r1fq4yp*oyskexrv$_#2#*elbyzoh&_45c@schpaihsfvynx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [ "secteller.onrender.com", "127.0.0.1"]
+ALLOWED_HOSTS = [ "secteller.onrender.com"]
 
 
 # Application definition
